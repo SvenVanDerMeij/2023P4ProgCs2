13795183e24ed33966b648e2c13c4dda910e9dc8
SvenVanDerMeij <38007@ma-web.nl>
2024-05-17 12:30:46 +0200

    wat is het punt van het leven?

--- ConsoleMonGame/Program.cs
+++ ConsoleMonGame/Program.cs
@@ -6,18 +6,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            TestConsoleMonFunctions();
+            TestSkillFunctions();
         }
-        static void TestConsoleMonFunctions()
+        static void TestSkillFunctions()
         {
-            Console.WriteLine("TestConsoleMonFunctions");
-            ConsoleMon mon = new ConsoleMon();
-            mon.TakeDamage(100);
-            mon.DepleteEnergy(20);
+            Console.WriteLine("TestSkillFunctions");
+            ConsoleMon casterMon = new ConsoleMon();
+            ConsoleMon targetMon = new ConsoleMon();
+            Skill skill = new Skill()
+            {
+                damage = 100,
+                energyCost = 20,
+            };
+            skill.UseOn(targetMon, casterMon);
 
-            Console.WriteLine(mon.health == -100);
+            Console.WriteLine(targetMon.health == -100);
 
-            Console.WriteLine(mon.energy == -20);
+            Console.WriteLine(casterMon.energy == -20);
         }
 
     }
