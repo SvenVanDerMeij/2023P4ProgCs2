5ac7c8becaea99788e08748923e05e95218c5cc3
SvenVanDerMeij <38007@ma-web.nl>
2024-06-19 21:00:57 +0200

    hallo episode V: The empire strikes back

---
+++ SplitOpdracht/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SplitOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = File.ReadAllText("stringsplit.txt");
+            string[] keyvalue = content.Split(new char[] {  ':', ',' } );
+            
+            Console.WriteLine(keyvalue[0]);
+            Console.WriteLine(keyvalue[1]);
+            Console.WriteLine(keyvalue[2]);
+
+
+        }
+    }
+}
